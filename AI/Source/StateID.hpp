9a4e2fad0bca5430a63c6c67eb230ad0ec323cf7
Henry <193541T@mymail.nyp.edu.sg>
2021-01-26 17:20:25 +0800

    Update StateID.hpp

--- AI/Source/StateID.hpp
+++ AI/Source/StateID.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
 enum struct StateID: int{
+	StateIdleKnight,
+	StatePatrolKnight,
+	StateChaseKnight,
+	StateAttackKnight,
+	StateEscapeKnight,
+	StateDeadKnight,
+
+	StateIdleGunner,
+	StatePatrolGunner,
+	StateChaseGunner,
+	StateAttackGunner,
+	StateEscapeGunner,
+	StateDeadGunner,
+
+	StateWanderHealer,
+	StateFollowHealer,
+	StateHealHealer,
+	StateDeadHealer,
+
 	Amt
 };
